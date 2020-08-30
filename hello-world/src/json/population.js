69d6a8ae7570ee3a198a606e562bc4ab7d64ba0a
khadir7 <khadirahmed.sd@gmail.com>
2020-08-30 21:19:06 +0530

    json changes

---
+++ hello-world/src/json/population.js
@@ -0,0 +1,87 @@
+export default {
+    2016:[
+        {
+          country: "Bangladesh",
+          population: 161356039,
+        },
+        {
+          country: "Brazil",
+          population: 209469333,
+        },
+        {
+          country: "China",
+          population: 1392730000,
+        },
+        {
+          country: "Czech Republic",
+          population: 10629928,
+        },
+    
+        {
+          country: "Egypt",
+          population: 98423595,
+        },
+    
+        {
+          country: "Germany",
+          population: 82905782,
+        },
+    
+        {
+          country: "India",
+          population: 1352617328,
+        },
+        {
+          country: "Indonesia",
+          population: 267663435,
+        },
+        {
+          country: "Iran",
+          population: 81800269,
+        },
+        {
+          country: "Iraq",
+          population: 38433600,
+        },
+        {
+          country: "Japan",
+          population: 126529100,
+        },
+        {
+          country: "Pakistan",
+          population: 212215030,
+        },
+        {
+          country: "Philippines",
+          population: 106651922,
+        },
+        {
+          country: "Russian Federation",
+          population: 144478050,
+        },
+        {
+          country: "Thailand",
+          population: 69428524,
+        },
+        {
+          country: "The Democratic Rep. Congo",
+          population: 84068091,
+        },
+        {
+          country: "Mexico",
+          population: 126190788,
+        },
+        {
+          country: "Turkey",
+          population: 82319724,
+        },
+        {
+          country: "United Kingdom",
+          population: 66460344,
+        },
+        {
+          country: "United States",
+          population: 326687501,
+        },
+      ]
+}
