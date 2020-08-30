808d937b58729fcbafff4dafe8fcbd80e126a2c3
khadir7 <khadirahmed.sd@gmail.com>
2020-08-30 21:16:44 +0530

    bar changes

--- hello-world/src/pages/bars.js
+++ hello-world/src/pages/bars.js
@@ -1,116 +1,131 @@
 import React, { useState, useEffect } from "react"
 import styled from "styled-components"
+import population from "../json/population"
 
-const CountryCell = styled.td`
-  display: inline-block;
+
+const CountryCell = styled.div`
+  
   width: 250px;
   text-align: center;
 `
-const CountryRow = styled.tr`
+const CountryRow = styled.div`
+display: flex;
+align-items:center;
+width: 100%;
+padding: 0px 15px; 
   position: absolute;
   transition: all 0.2s linear;
   transform: translateY(${props => props.index * 20}px);
 `
 
-const CountCell = styled.td`
+const CountCell = styled.div`
   display: inline-block;
   text-align: right;
   width: 120px;
 `
+const Bar = styled.div`
+  flex-grow: 1;
+`
 
 const Bars = () => {
-  let obj1 = [
-    {
-      country: "Bangladesh",
-      population: 161356039,
-    },
-    {
-      country: "Brazil",
-      population: 209469333,
-    },
-    {
-      country: "China",
-      population: 1392730000,
-    },
-    {
-      country: "Czech Republic",
-      population: 10629928,
-    },
+  // let obj1 = population["2016"]
+  // let obj1 = [
+  //   {
+  //     country: "Bangladesh",
+  //     population: 161356039,
+  //   },
+  //   {
+  //     country: "Brazil",
+  //     population: 209469333,
+  //   },
+  //   {
+  //     country: "China",
+  //     population: 1392730000,
+  //   },
+  //   {
+  //     country: "Czech Republic",
+  //     population: 10629928,
+  //   },
 
-    {
-      country: "Egypt",
-      population: 98423595,
-    },
+  //   {
+  //     country: "Egypt",
+  //     population: 98423595,
+  //   },
 
-    {
-      country: "Germany",
-      population: 82905782,
-    },
+  //   {
+  //     country: "Germany",
+  //     population: 82905782,
+  //   },
 
-    {
-      country: "India",
-      population: 1352617328,
-    },
-    {
-      country: "Indonesia",
-      population: 267663435,
-    },
-    {
-      country: "Iran",
-      population: 81800269,
-    },
-    {
-      country: "Iraq",
-      population: 38433600,
-    },
-    {
-      country: "Japan",
-      population: 126529100,
-    },
-    {
-      country: "Pakistan",
-      population: 212215030,
-    },
-    {
-      country: "Philippines",
-      population: 106651922,
-    },
-    {
-      country: "Russian Federation",
-      population: 144478050,
-    },
-    {
-      country: "Thailand",
-      population: 69428524,
-    },
-    {
-      country: "The Democratic Rep. Congo",
-      population: 84068091,
-    },
-    {
-      country: "Mexico",
-      population: 126190788,
-    },
-    {
-      country: "Turkey",
-      population: 82319724,
-    },
-    {
-      country: "United Kingdom",
-      population: 66460344,
-    },
-    {
-      country: "United States",
-      population: 326687501,
-    },
-  ]
+  //   {
+  //     country: "India",
+  //     population: 1352617328,
+  //   },
+  //   {
+  //     country: "Indonesia",
+  //     population: 267663435,
+  //   },
+  //   {
+  //     country: "Iran",
+  //     population: 81800269,
+  //   },
+  //   {
+  //     country: "Iraq",
+  //     population: 38433600,
+  //   },
+  //   {
+  //     country: "Japan",
+  //     population: 126529100,
+  //   },
+  //   {
+  //     country: "Pakistan",
+  //     population: 212215030,
+  //   },
+  //   {
+  //     country: "Philippines",
+  //     population: 106651922,
+  //   },
+  //   {
+  //     country: "Russian Federation",
+  //     population: 144478050,
+  //   },
+  //   {
+  //     country: "Thailand",
+  //     population: 69428524,
+  //   },
+  //   {
+  //     country: "The Democratic Rep. Congo",
+  //     population: 84068091,
+  //   },
+  //   {
+  //     country: "Mexico",
+  //     population: 126190788,
+  //   },
+  //   {
+  //     country: "Turkey",
+  //     population: 82319724,
+  //   },
+  //   {
+  //     country: "United Kingdom",
+  //     population: 66460344,
+  //   },
+  //   {
+  //     country: "United States",
+  //     population: 326687501,
+  //   },
+  // ]
 
-  obj1.map((item, index) => {
-    item.index = index
-    return item
-  })
-  const [obj, setObj] = useState(obj1)
+  
+  const [obj, setObj] = useState(population["2016"])
   const [scale, setScale] = useState(1392730000)
+  useEffect(() => {
+    let obj1 = obj;
+    obj1.map((item, index) => {
+      item.index = index
+      return item
+    })
+    setObj(obj1)
+  },[])
   const onclick = () => {
     let obj2 = obj
       .slice()
@@ -130,13 +145,13 @@ const Bars = () => {
         <span>{`< `}</span>
         <span>{` >`}</span>
       </div>
-      <section style={{ position: "relative" }}>
-        <table>
-          <tbody>
-            {obj.map(country => (
-              <CountryRow index={country.index} key={country.country}>
+      <section style={{ position: "relative"}}>
+        {/* <table>
+          <tbody> */}
+            {obj.map((country, index) => (
+              <CountryRow index={country.index || index} key={country.country}>
                 <CountryCell>{country.country}</CountryCell>
-                <td style={{ display: "inline-block", width: "200px" }}>
+                <Bar >
                   <div
                     style={{
                       background: "blue",
@@ -145,12 +160,12 @@ const Bars = () => {
                       width: `${getBarWidth(country.population)}%`,
                     }}
                   ></div>
-                </td>
+                </Bar>
                 <CountCell>{country.population}</CountCell>
               </CountryRow>
             ))}
-          </tbody>
-        </table>
+          {/* </tbody>
+        </table> */}
       </section>
     </div>
   )
